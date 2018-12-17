6a005e9a1985ab22670fd356c25b5e0f85a49380
sunyx <yong_xing2008@126.com>
2018-12-17 22:43:34 +0800

    first commit

---
+++ src/main/java/com/sunyx/test/HelloAction.java
@@ -0,0 +1,11 @@
+package com.sunyx.test;
+
+/**
+ * Created by sunyx on 2018/12/14.
+ */
+public class HelloAction {
+    public String hello() {
+        System.out.printf("欢迎你");
+        return "success";
+    }
+}
